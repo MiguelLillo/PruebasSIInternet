a073016ac19e702fbc25e33ae66aa7af420afd7c
MiguelLillo <miguel_lillo96@hotmail.com>
2018-05-07 17:55:21 +0200

    Anadimos login.java e usuario.java

--- src/java/paquete1/Usuario.java
+++ src/java/paquete1/Usuario.java
@@ -2,7 +2,7 @@
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
  */
-package es.uma.informatica.sii.jsf.autenticacion.modelo;
+package paquete1;
 
 import java.io.Serializable;
 import javax.persistence.Entity;
